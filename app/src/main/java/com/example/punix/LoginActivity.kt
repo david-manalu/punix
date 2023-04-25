f91791c97ac8c28d0f8dbb16f34c2621e0586883
StefanusTitan <stefanustitan@gmail.com>
2023-04-25 18:23:47 +0700

    Progress 2

--- app/src/main/java/com/example/punix/LoginActivity.kt
+++ app/src/main/java/com/example/punix/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var auth: FirebaseAuth
     private lateinit var email: EditText
     private lateinit var password: EditText
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
@@ -21,8 +23,11 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         auth = FirebaseAuth.getInstance()
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.login_button)
 
-        signIn(email.text.toString(), password.text.toString())
+        btnLogin.setOnClickListener {
+            signIn(email.text.toString(), password.text.toString())
+        }
     }
 
     public override fun onStart() {
@@ -59,13 +64,12 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         user?.let {
             val intent = Intent(this@LoginActivity, MainActivity::class.java)
             startActivity(intent)
-        } ?: run {
-            reload()
         }
     }
 
     private fun reload() {
         finish()
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
         startActivity(intent)
     }
 
